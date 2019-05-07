18f47b348fef5b611a563db03a9d2474c01b4a54
Daniel Beck <daniel-beck@users.noreply.github.com>
2019-05-08 00:12:29 +0200

    Initial commit

---
+++ src/main/java/io/jenkins/plugins/localization_demo/LocalizationContributorImpl.java
@@ -0,0 +1,9 @@
+package io.jenkins.plugins.localization_demo;
+
+import hudson.Extension;
+import io.jenkins.plugins.localization.support.LocalizationContributor;
+
+@Extension
+public class LocalizationContributorImpl extends LocalizationContributor {
+    // no content needed
+}
